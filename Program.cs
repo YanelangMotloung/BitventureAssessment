d5d46dfeb1d89b513b3af78452f7188224b22fda
Yanelang Motloung <yanelang@gmail.com>
2021-09-08 19:01:22 +0200

    Refactot(OpenFielDialog): OpenFiel Dialog added
    OpenFileDialog was added on the Main method to ensure the file directory.
    
    Affected: Program.cs

--- Program.cs
+++ Program.cs
@@ -10,16 +10,30 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
-using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace BitventureAssessment
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            string _filePath = string.Empty;
             JsonProcessor jsonProcessor = new JsonProcessor();
-            jsonProcessor.Process(@"C:\Users\DELL\Downloads\bonus_endpoints.json");
+
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "JSON files (*.json)|*.json";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    jsonProcessor.Process(openFileDialog.FileName);
+                }
+            }
+
             Console.ReadLine();
         }
     }
